dc921c52d7529d7e12b4e69d38dc0a594522017d
Naveen Kumar Singh <90003399+naveenks2002@users.noreply.github.com>
2025-05-05 10:03:11 +0530

    Add files via upload

---
+++ Tasks/Banking_Application/Banking_Application/BankAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Banking_Application
+{
+    internal class BankAccount
+    {
+        public int AccountNumber { get; set; }
+        public string AccountHolder { get; set; }
+        public decimal Balance { get; private set; }
+
+        public BankAccount(int accountNumber, string accountHolder, decimal initialBalance) {
+                AccountNumber = accountNumber;
+                AccountHolder = accountHolder;
+                Balance = initialBalance;   
+        }
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+            Console.WriteLine($"Deposited {amount:C}. New balance is {Balance:C}.");
+        }
+        public bool Withdraw(decimal amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Withdrew {amount:C}. New balance is {Balance:C}.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Funds");
+                return false;
+            }
+
+        }
+        public void DisplayBalance()
+        {
+            Console.WriteLine($"Account {AccountNumber} balance: {Balance:C} ");
+        }
+ 
+    }
+}
