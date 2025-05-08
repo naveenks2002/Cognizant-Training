050fc4428b33fcfcb1f89a925a317f9d2faad865
Naveen Kumar Singh <90003399+naveenks2002@users.noreply.github.com>
2025-05-08 09:59:07 +0530

    Add files via upload

---
+++ Tasks/Banking_Application/Banking_Application/BankAccount.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Banking_Application
+{
+    internal class BankAccount
+    {
+        public int AccountNumber { get; set; }
+        public string AccountHolder { get; set; }
+        public decimal Balance { get; private set; }
+
+        private static string connectionString= "Data Source=LTIN593398\\SQLEXPRESS;Integrated Security=True";
+
+        public BankAccount(int accountNumber, string accountHolder, decimal initialBalance)
+        {
+            AccountNumber = accountNumber;
+            AccountHolder = accountHolder;
+            Balance = initialBalance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+            UpdateBalanceInDatabase();
+            Console.WriteLine($"Deposited {amount:C}. New balance is {Balance:C}.");
+        }
+
+        public bool Withdraw(decimal amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+                UpdateBalanceInDatabase();
+                Console.WriteLine($"Withdrew {amount:C}. New balance is {Balance:C}.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Funds");
+                return false;
+            }
+        }
+
+        public void DisplayBalance()
+        {
+            Console.WriteLine($"Account {AccountNumber} balance: {Balance:C} ");
+        }
+
+        public void SaveToDatabase()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "INSERT INTO BankAccounts (AccountNumber, AccountHolderName, Balance) VALUES (@AccountNumber, @AccountHolderName, @Balance)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@AccountNumber", AccountNumber);
+                    command.Parameters.AddWithValue("@AccountHolderName", AccountHolder);
+                    command.Parameters.AddWithValue("@Balance", Balance);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void UpdateBalanceInDatabase()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "UPDATE BankAccounts SET Balance = @Balance WHERE AccountNumber = @AccountNumber";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Balance", Balance);
+                    command.Parameters.AddWithValue("@AccountNumber", AccountNumber);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static BankAccount GetAccountFromDatabase(int accountNumber)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM BankAccounts WHERE AccountNumber = @AccountNumber";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@AccountNumber", accountNumber);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string accountHolder = reader["AccountHolderName"].ToString();
+                            decimal balance = (decimal)reader["Balance"];
+                            return new BankAccount(accountNumber, accountHolder, balance);
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Tasks/Banking_Application/Banking_Application/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Banking_Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int accountNumber;
+           while (true)
+            {
+                Console.WriteLine("Welcome to Zyskee Banking Systems");
+                Console.WriteLine("1. Create Account");
+                Console.WriteLine("2. Deposit Money");
+                Console.WriteLine("3. Withdraw Money");
+                Console.WriteLine("4. View Balance");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("Choose an option: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter account holder name : ");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Enter Initial Deposit : ");
+                        decimal initialDeposit = decimal.Parse(Console.ReadLine());
+                        Random rnd = new Random();
+                        accountNumber = rnd.Next(5000, 6000);
+                        BankAccount newAccount = new BankAccount(accountNumber, name, initialDeposit);
+                        newAccount.SaveToDatabase();
+                        Console.WriteLine($"Account number successfully created with {newAccount.AccountNumber}.");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Enter Account Number: ");
+                        accountNumber = int.Parse(Console.ReadLine());
+                        BankAccount account = BankAccount.GetAccountFromDatabase(accountNumber);
+                        if (account != null)
+                        {
+                            Console.WriteLine("Enter deposit amount: ");
+                            decimal depositAmount = decimal.Parse(Console.ReadLine());
+                            account.Deposit(depositAmount);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Account not Found");
+                        }
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Enter Account Number: ");
+                        accountNumber = int.Parse(Console.ReadLine());
+                        account = BankAccount.GetAccountFromDatabase(accountNumber);
+                        if (account != null)
+                        {
+                            Console.WriteLine("Enter Withdrawal Amount: ");
+                            decimal withdrawAmount = decimal.Parse(Console.ReadLine());
+                            account.Withdraw(withdrawAmount);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Account not Found");
+                        }
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Enter Account Number: ");
+                        accountNumber = int.Parse(Console.ReadLine());
+                        account = BankAccount.GetAccountFromDatabase(accountNumber);
+                        if (account != null)
+                        {
+                            account.DisplayBalance();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Account Not Found");
+                        }
+                        break;
+
+                    case 5:
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid Option. Please Try Again!");
+                        break;
+                }
+            }
+        }
+    }
+}
